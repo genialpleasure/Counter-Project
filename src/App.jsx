d2384522d77200d45210d457bf862c97cce22bba
genialpleasure <isaacnpleasure01@gmail.com>
2025-12-14 23:19:52 +0100

    using callback hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import ConfigureCounter from './components/Counter/ConfigureCounter.jsx';
 import Counter from './components/Counter/Counter.jsx';
 import Header from './components/Header.jsx';
 import { log } from './log.js';
@@ -7,13 +8,17 @@ import { log } from './log.js';
 function App() {
   log('<App /> rendered');
 
+  const [chosenCount, setChosenCount] = useState(0);
 
+  function handleSetCount(newCount) {
+    setChosenCount(newCount);
+  }
 
   return (
     <>
       <Header />
       <main>
-        
+        <ConfigureCounter onSet={handleSetCount} />
         <Counter initialCount={chosenCount} />
       </main>
     </>
